efaaefe989acf7bbdd1d6670427b7a98a0aace82
Wilbert Caba <wilbertcaba@gmail.com>
2025-02-06 01:21:45 -0500

    Add firstblock and boilerplate as submodules

---
+++ themes/blocks-theme/inc/wpcom.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * WordPress.com-specific functions and definitions
+ *
+ * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
+ *
+ * @package Blocks-Theme 
+ */
+
+/**
+ * Adds support for wp.com-specific theme functions.
+ *
+ * @global array $themecolors
+ */
+function _blocks_theme_wpcom_setup() {
+	global $themecolors;
+
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		// Whitelist wpcom specific variable intended to be overruled.
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+		$themecolors = array(
+			'bg'     => '',
+			'border' => '',
+			'text'   => '',
+			'link'   => '',
+			'url'    => '',
+		);
+	}
+}
+add_action( 'after_setup_theme', '_blocks_theme_wpcom_setup' );
